f90b50aa6ad0f10a0b69cc73baf49552e6b1041e
Mrinal Wahal <mrinal_w@rocketmail.com>
2023-05-14 23:48:54 +0530

    chore: gh action failure

--- internal/integrations/internal/gsm/ops.go
+++ internal/integrations/internal/gsm/ops.go
@@ -6,7 +6,6 @@ import (
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	"github.com/envsecrets/envsecrets/internal/context"
-	"google.golang.org/api/iterator"
 )
 
 func CreateSecret(ctx context.ServiceContext, client *secretmanager.Client, parent string, secretID string) (string, error) {
@@ -51,31 +50,4 @@ func AddSecretVersion(ctx context.ServiceContext, client *secretmanager.Client, 
 	}
 
 	return result.Name, nil
-}
-
-func ListSecrets(ctx context.ServiceContext, client *secretmanager.Client, parent string) (secrets []string, errors []error) {
-
-	// Build the request.
-	req := &secretmanagerpb.ListSecretsRequest{
-		Parent: parent,
-	}
-
-	// Call the API.
-	it := client.ListSecrets(ctx, req)
-
-	for {
-		resp, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-
-		if err != nil {
-			errors = append(errors, err)
-			secrets = append(secrets, "")
-			continue
-		}
-		secrets = append(secrets, resp.Name)
-		errors = append(errors, nil)
-	}
-	return secrets, errors
 }
